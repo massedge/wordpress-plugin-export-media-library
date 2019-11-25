398cf7f9476adacd3894c3f6d23b1f9f8f6848f4
Andrej Pavlovic <andrej.pavlovic@massedge.com>
2019-11-25 12:28:20 -0500

    Only require locale autoloader, if the main plugin class is not already accessible (such as with another autoloader)

--- run.php
+++ run.php
@@ -3,8 +3,12 @@
 // must be loaded by the plugin index.php file
 if (!defined('MASSEDGE_WORDPRESS_PLUGIN_EXPORT_MEDIA_LIBRARY_PLUGIN_PATH')) exit;
 
-//require 'lib/autoload.php';
-require 'vendor/autoload.php';
+// If main plugin class can't be found, use the local autoloader. Generally,
+// local autoloader would not be required if the plugin is installed using
+// composer, while a regular wordpress install will need the local autoloader.
+if (!class_exists('MassEdge\\WordPress\\Plugin\\ExportMediaLibrary\\Plugin')) {
+    require 'vendor/autoload.php';
+}
 
 (new MassEdge\WordPress\Plugin\ExportMediaLibrary\Plugin([
     'pluginPath' => MASSEDGE_WORDPRESS_PLUGIN_EXPORT_MEDIA_LIBRARY_PLUGIN_PATH,
